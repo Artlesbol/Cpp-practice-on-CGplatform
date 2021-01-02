a4dc038646ff1ccfdb211a4974b03c7346efccc9
Artlesbol <74887228+Artlesbol@users.noreply.github.com>
2021-01-02 15:55:38 +0800

    Update 9-11.cpp

--- tasks of the 9th week/9-11.cpp
+++ tasks of the 9th week/9-11.cpp
@@ -11,26 +11,26 @@ using namespace std;
 
 int main()
 {
-    bool monkey[1000]={0};
-    int m,n,num,p=0;
+    bool monkey[1000]={0};//标记猴子是否被淘汰
+    int m,n,num,p=0;//num为猴子总数的拷贝用于记录剩余猴子，p为循环标记为指向的当前猴子
     cin>>m>>n;
     num=m;
-    int count=1;
+    int count=1;//计数器 count不断增加 每数到n时淘汰当前的猴子
     while (num!=1)
     {
         if(count==n)
         {
             count=1;
-            monkey[p]=1;
+            monkey[p]=1;//淘汰猴子p
             num--;
         }
         if(monkey[p]==1)
         {
-            p=(p+1)%m;
+            p=(p+1)%m;//遇到已淘汰的猴子则跳过
             continue;
         }
         p=(p+1)%m;
-        count++;
+        count++;//遇到没淘汰则计数器计数+1
     }
     for(int i=0;i<1000;i++)
     {
